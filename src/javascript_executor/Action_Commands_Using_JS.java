29499fe11d3f6896a8b6eb1f61ab395581fabe16
SiripuramRajesh <siripuramrajeswararao@gmail.com>
2021-02-15 13:56:46 +0530

    FirstCommit

---
+++ src/javascript_executor/Action_Commands_Using_JS.java
@@ -0,0 +1,50 @@
+package javascript_executor;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Action_Commands_Using_JS 
+{
+
+	public static void main(String[] args) throws Exception 
+	{
+		
+		System.setProperty("webdriver.chrome.driver", "Drivers\\chromedriver.exe");    
+		//browser initiation command
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		driver.manage().window().maximize();
+		
+		
+		//Enable Javascript on automation browser
+		JavascriptExecutor js=((JavascriptExecutor)driver);
+		
+		//Type text into editbox
+		js.executeScript("document.getElementById('email').value='9030248855'");
+		js.executeScript("document.getElementById('pass').value='Hello123456'");
+		
+	
+		//select dropdown with value property using javascript 
+		js.executeScript("document.getElementById('day').value='5'");
+		//select dropdown with Index number  using javascript
+		js.executeScript("document.getElementById('month').selectedIndex ='06'");
+		
+		
+		//javascript selecting radio button
+		WebElement Female_Rbtn=driver.findElement(By.xpath("//input[@value='1']"));
+		js.executeScript("arguments[0].checked='true'", Female_Rbtn);
+		Thread.sleep(3000);
+		
+		//javascript selecting radio button
+		WebElement Male_Rbtn=driver.findElement(By.xpath("//input[@value='2']"));
+		js.executeScript("arguments[0].click()", Male_Rbtn);
+		
+		//Perform click action using javascript on button..
+		WebElement Signup_btn=driver.findElement(By.xpath("//button[@name='websubmit']"));
+		js.executeScript("arguments[0].click()",Signup_btn);
+	}
+
+}
---
+++ src/pageobjects/PageFactory/Run_CT_SearchFlight_Objects.java
@@ -0,0 +1,36 @@
+package pageobjects.PageFactory;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class Run_CT_SearchFlight_Objects 
+{
+
+	public static void main(String[] args) throws Exception 
+	{
+		//Set Runtime environment variable for chrome driver
+		String chrome_path="Drivers\\chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", chrome_path);
+				
+		
+		//browser initiation
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://cleartrip.com");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		Thread.sleep(5000);
+		
+		
+		/*
+		 * Inorder to get @FindBy annotated object we should take help of
+		 * pagefactory class.
+		 */
+		 CT_Search_Flights Flights=PageFactory.initElements(driver, CT_Search_Flights.class);
+		 Flights.Verify_Expected_Header_Visibility();
+		 Flights.flight_search_oneway_trip();
+
+	}
+
+}
